80b56af3ac0d132f26dd2449b905df2fc7aced80
Montse <zaratemontserrat74@gmail.com>
2026-03-04 21:56:37 -0600

    Ejercicio 4 Figuras

---
+++ app/lib/src/main/java/com/pajasoft/lib/Figuras.kt
@@ -0,0 +1,37 @@
+package com.pajasoft.lib
+
+interface Figuras{
+    fun calcularArea(): Double
+    fun calcularPerimetro(): Double
+}
+
+class Rectangulo(val base: Double, val altura: Double) : Figuras{
+
+    override fun calcularArea(): Double = base * altura
+
+    override fun calcularPerimetro(): Double {
+        return 2 * (base + altura)
+    }
+}
+
+class Circulo(val radio: Double) : Figuras {
+
+    override fun calcularArea(): Double {
+        return Math.PI * radio * radio
+    }
+
+    override fun calcularPerimetro(): Double {
+        return 2 * Math.PI * radio
+    }
+}
+
+fun main() {
+    val rect = Rectangulo(5.0, 3.0)
+    val circ = Circulo(4.0)
+
+    println("Rectángulo - Área: %.2f, Perímetro: %.2f"
+        .format(rect.calcularArea(), rect.calcularPerimetro()))
+
+    println("Círculo - Área: %.2f, Perímetro: %.2f"
+        .format(circ.calcularArea(), circ.calcularPerimetro()))
+}
